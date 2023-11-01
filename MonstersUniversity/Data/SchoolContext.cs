f919ad3d826db8784c89c10bda59a010137edb42
elliotallen1 <allened@hendrix.edu>
2023-11-01 17:36:35 -0500

    Added database stuff
    Database is up and working! Have a starter db that displays

--- MonstersUniversity/Data/SchoolContext.cs
+++ MonstersUniversity/Data/SchoolContext.cs
@@ -13,6 +13,16 @@ namespace MonstersUniversity.Data
             : base(options)
         {
         }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
 
         public DbSet<MonstersUniversity.Models.Student> Student { get; set; } = default!;
     }
